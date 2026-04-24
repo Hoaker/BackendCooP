99bbd685950da47a1a896a76153a880bd2bc92fb
root <root@Beacon.Runner>
2026-04-24 17:43:24 +0100

    Fix bcrypt 72-byte truncation error

--- security.py
+++ security.py
@@ -9,6 +9,11 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 120
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", truncate_error=True)
+
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
